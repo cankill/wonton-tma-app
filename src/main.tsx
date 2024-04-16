8b69e8fee1dff9140fdb3ccba090e86be636f297
Andrey Filyanin <filyaniny@gmail.com>
2024-04-16 22:00:20 +0400

    Move manifest.json

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from './App.tsx'
 import './index.css'
 import { TonConnectUIProvider } from '@tonconnect/ui-react'
 
-const manifestUrl = "https://raw.githubusercontent.com/markokhman/func-course-chapter-5-code/master/public/manifest.json";
+const manifestUrl = "https://raw.githubusercontent.com/cankill/sc-counter/main/public/tonconnect-manifest.json";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
