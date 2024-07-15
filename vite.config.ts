f8cb9360cee2c8b73d165581792c6106da933e5b
Andrey Filyanin <filyaniny@gmail.com>
2024-07-15 20:28:50 +0400

    TMA1

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), nodePolyfills() ],
-  base: "/sc-counter/"
+  plugins: [react(), basicSsl() ],
+  build: {
+    outDir: './docs'
+  },
+  base: "./"
 })
