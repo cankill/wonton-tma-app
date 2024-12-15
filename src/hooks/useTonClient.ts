028cf87253d6f1b274e6d1561adce995c0bdacb9
SimpleMoves <simplemoves.soft@gmail.com>
2024-12-15 12:57:33 +0400

    v0.0.41 Clean the names

--- src/hooks/useTonClient.ts
+++ src/hooks/useTonClient.ts
@@ -1,7 +1,10 @@
 import { getHttpEndpoint } from "@orbs-network/ton-access";
 import { useAsyncInitialize } from "./useAsyncInitialize.ts";
 import { TonClient } from "@ton/ton";
+import { testOnly } from "../store/NftsStore.ts";
 
 export function useTonClient() {
-    return useAsyncInitialize(async () => new TonClient({ endpoint: await getHttpEndpoint({network: "testnet" }) }));
+    return useAsyncInitialize(async () => new TonClient({ endpoint: await getHttpEndpoint({
+            network: testOnly ? "testnet" : "mainnet"
+    }) }));
 }
